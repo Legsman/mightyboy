eac7253127dfc4344ac2984d701ad6e29d8da516
Antoine <maciaantoine@yahoo.fr>
2016-09-04 06:56:28 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mightyboy::Application.config.secret_key_base = 'a5f62ac05f304a527b0385a4c81efac90b8db292fdb73c01afc390bc6384f0d7d61166de2e9cf0a497660db9e9ac5d977fd5a2d2ff2c3c8fedf43103802e356f'
