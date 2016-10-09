07780548e354b12b52e1f7d2f28efe9e98c2c48b
Antoine <maciaantoine@yahoo.fr>
2016-10-09 07:36:45 +0000

    added new model for shopping cart

--- db/migrate/20160904102755_create_orders.rb
+++ db/migrate/20160904102755_create_orders.rb
@@ -3,6 +3,9 @@ class CreateOrders < ActiveRecord::Migration
     create_table :orders do |t|
       t.string :name
       t.string :email
+      t.decimal :subtotal
+      t.decimal :tax
+      t.decimal :total
       t.datetime :pickup_time
       t.string :phone
       t.text :info   
